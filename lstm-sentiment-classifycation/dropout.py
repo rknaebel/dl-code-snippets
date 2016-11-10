3fe7af00edffcc2a512423f614b833e37b0e3228
Rene Knaebel <reneknaebel90@gmail.com>
2016-11-10 13:40:34 +0100

    add sentiment classifycation with lstm and conv, add object recoginition on cifar

--- lstm-sentiment-classifycation/dropout.py
+++ lstm-sentiment-classifycation/dropout.py
@@ -1,18 +1,17 @@
-# LSTM for sequence classification in the IMDB dataset
+# LSTM with Dropout for sequence classification in the IMDB dataset
 import numpy
 from keras.datasets import imdb
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
+from keras.layers import Dropout
 from keras.layers.embeddings import Embedding
 from keras.preprocessing import sequence
 # fix random seed for reproducibility
 numpy.random.seed(7)
-
 # load the dataset but only keep the top n words, zero the rest
 top_words = 5000
 (X_train, y_train), (X_test, y_test) = imdb.load_data(nb_words=top_words)
-
 # truncate and pad input sequences
 max_review_length = 500
 X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
@@ -21,8 +20,10 @@ X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
 # create the model
 embedding_vecor_length = 32
 model = Sequential()
-model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
+model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length, dropout=0.2))
+model.add(Dropout(0.2))
 model.add(LSTM(100))
+model.add(Dropout(0.2))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 print(model.summary())
